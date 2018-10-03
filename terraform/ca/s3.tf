ebc8b03ce87beee2cf5093871bbe93f7646b06bb
Dan Williams <dan@ma.ssive.co>
2018-10-02 22:56:52 -0400

    make bucketname more unique

--- terraform/ca/s3.tf
+++ terraform/ca/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "private" {
-  bucket = "serverlessl-${sha1(data.aws_caller_identity.current.account_id)}"
+  bucket = "serverlessl-${var.ca_name}-${sha1(data.aws_caller_identity.current.account_id)}"
   acl    = "private"
 }
 
