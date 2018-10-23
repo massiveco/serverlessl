4b3f1f4c4ef626b087ae48f04f16d5e449969b7a
Dan Williams <dan@ma.ssive.co>
2018-10-22 20:40:11 -0400

    export bucket name and arn

--- terraform/ca/_output.tf
+++ terraform/ca/_output.tf
@@ -2,6 +2,10 @@ output "s3_bucket" {
   value = "${aws_s3_bucket.private.id}"
 }
 
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.private.arn}"
+}
+
 output "lambda_ca_arn" {
   value = "${aws_lambda_function.ca.arn}"
 }
